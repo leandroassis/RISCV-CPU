527e62eac6f7c2efb34c3e0b948149ec8f156dab
Leandro Assis dos Santos <assissantosleandro@poli.ufrj.br>
2023-06-28 01:32:35 -0300

    +pc

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ Flags = --std=08
 
 all:
 	ghdl -a $(Flags) $(SRC)
-	ghdl -e $(Flags) test
-	ghdl -r $(Flags) test --wave=testbench.ghw --stop-time=10us
+	ghdl -e $(Flags) test_pc
+	ghdl -r $(Flags) test_pc --wave=testbench.ghw --stop-time=10us
 	mv testbench.ghw ./test/waveforms/testbench.ghw
---
+++ src/prog_count.vhd
@@ -0,0 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity prog_count is
+    port(
+        clk : in std_logic;
+        imm : in std_logic_vector(31 downto 0);
+        branch : in std_logic;
+        pc : out std_logic_vector(31 downto 0)
+    );
+end entity;
+
+architecture Behavioral of prog_count is
+    signal immediate, ptr_pc : std_logic_vector(31 downto 0) := (others => '0');
+    begin 
+        immediate <= imm sll 1;
+        pc <= ptr_pc;
+
+        fetch_instr: process(clk, ptr_pc) is
+        begin
+            if rising_edge(clk) then
+                if branch = '1' then
+                    ptr_pc <= std_logic_vector(unsigned(ptr_pc) + unsigned(immediate));
+                else
+                    ptr_pc <= std_logic_vector(unsigned(ptr_pc) + 4);
+                end if;
+            end if;
+        end process fetch_instr;
+end Behavioral;
