0e6e54ad7f023d583b88cc8c3f94fe839e33443e
Leandro Assis dos Santos <assissantosleandro@poli.ufrj.br>
2023-07-08 20:53:28 -0300

    adicionando alu

--- src/alu_ctrl.vhd
+++ src/alu_ctrl.vhd
@@ -9,10 +9,36 @@ entity alu_ctrl is
     );
 end entity;
 
-architecture Behavioral of alu_ctrl is
+-- Operação  OPCODE  Br_aux
+-- nop        0000     0
+-- Soma       0001     0 
+-- Sub        0010     0
+-- and        0011     0
+-- or         0100     0
+-- xor        0101     0
+-- slt        0110     0
+-- srl        0111     0
+-- equal      1000     1 if equal
+-- ~equal     1001     1 if not equal
+-- in1 + 4    1010     1
+-- out = in2  1011     0
 
-    signal 
+architecture Behavioral of alu_ctrl is
+    signal funct3 : std_logic_vector(2 downto 0) := (others => '0');
+    signal funct7 : std_logic_vector(6 downto 0) := (others => '0');
+
+    function R_Type(signal funct3 : in )
 
 begin
+
+    funct3 <= instruction(14 downto 12);
+    funct7 <= instruction(31 downto 25);
+
+    op_decode: process(alu_op) is
+        begin
+            if alu_op = "001" then
+                
+            end if;
+    end process op_decode;
 
 end Behavioral;
