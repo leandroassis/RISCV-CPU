f360919d87b01bc5afa0b55cb807bac4b9774403
Leandro Assis <73458591+leandroassis@users.noreply.github.com>
2023-07-08 19:39:16 -0300

    Update prog_count.vhd

--- src/prog_count.vhd
+++ src/prog_count.vhd
@@ -7,6 +7,8 @@ entity prog_count is
         clk : in std_logic;
         imm : in std_logic_vector(31 downto 0);
         branch : in std_logic;
+        pc_reg_mux : in std_logic;
+        reg_in : in std_logic_vector(31 downto 0);
         pc : out std_logic_vector(31 downto 0)
     );
 end prog_count;
@@ -14,16 +16,20 @@ end prog_count;
 architecture Behavioral of prog_count is
     signal immediate, ptr_pc : std_logic_vector(31 downto 0) := (others => '0');
     begin 
-        immediate <= imm; -- precisa shiftar dnv?
+        immediate <= imm;
         pc <= ptr_pc;
 
         fetch_instr: process(clk, ptr_pc) is
         begin
             if rising_edge(clk) then
                 if branch = '1' then
-                    ptr_pc <= std_logic_vector(unsigned(ptr_pc) + unsigned(immediate));
+                	if pc_reg_mux = '1' then
+                    	ptr_pc <= std_logic_vector(unsigned(ptr_pc) + unsigned(immediate));
+                    else
+                    	ptr_pc <= std_logic_vector(unsigned(reg_in) + unsigned(immediate));
+                    end if;
                 else
-                    ptr_pc <= std_logic_vector(unsigned(ptr_pc) + 4);
+                    ptr_pc <= std_logic_vector(unsigned(ptr_pc) + 1);
                 end if;
             end if;
         end process fetch_instr;
