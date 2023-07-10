109bda32da8948525370033f3ece199a4ca05e99
Leandro Assis <73458591+leandroassis@users.noreply.github.com>
2023-07-10 11:30:35 -0300

    Update alu_ctrl.vhd

--- src/alu_ctrl.vhd
+++ src/alu_ctrl.vhd
@@ -28,30 +28,6 @@ architecture Behavioral of alu_ctrl is
     signal funct3 : std_logic_vector(2 downto 0) := (others => '0');
     signal funct7 : std_logic_vector(6 downto 0) := (others => '0');
 
-    function logic_arith_type(func3 : std_logic_vector(2 downto 0); func7 : std_logic_vector(6 downto 0)) return std_logic_vector is
-        variable opcode_temp : std_logic_vector(3 downto 0);
-        begin
-            if func3 = "000" then
-                if func7 = "0000000" then -- add
-                    opcode_temp := "0001";
-                elsif func7 = "0100000" then -- sub
-                    opcode_temp := "0010";
-                end if;
-            elsif func3 = "001" then -- sll
-                opcode_temp := "0110";
-            elsif func3 = "100" then -- xor
-                opcode_temp := "0101";
-            elsif func3 = "101" then -- srl
-                opcode_temp := "0111";
-            elsif func3 = "110" then -- or
-                opcode_temp := "0100";
-            elsif func3 = "111" then -- and
-                opcode_temp := "0011";
-            end if;
-
-            return opcode_temp;
-    end function;
-
 begin
 
     funct3 <= instruction(14 downto 12);
@@ -60,7 +36,25 @@ begin
     op_decode: process(alu_op, funct3, funct7) is
         begin
             if alu_op = "001" or alu_op = "010" then
-                opcode <= logic_arith_type(funct3, funct7);
+                if funct3 = "000" and alu_op = "001" then
+	                if funct7 = "0000000" then -- add
+	                    opcode <= "0001";
+	                elsif funct7 = "0100000" then -- sub
+	                    opcode <= "0010";
+	                end if;
+	           elsif funct3 = "000" and alu_op = "010" then
+	           	opcode <= "0001";
+	           elsif funct3 = "001" then -- sll
+	                opcode <= "0110";
+	           elsif funct3 = "100" then -- xor
+	                opcode <= "0101";
+	           elsif funct3 = "101" then -- srl
+	                opcode <= "0111";
+	           elsif funct3 = "110" then -- or
+	                opcode <= "0100";
+	           elsif funct3 = "111" then -- and
+	                opcode <= "0011";
+	           end if;
             elsif alu_op = "011" then
                 if funct3 = "000" then -- beq
                     opcode <= "1000";
