d435d7460db11e4592ce8b026f7de911f482cb39
Leandro Assis <73458591+leandroassis@users.noreply.github.com>
2023-07-12 12:36:00 -0300

    Update fwd_unit.vhd

--- src/fwd_unit.vhd
+++ src/fwd_unit.vhd
@@ -2,40 +2,49 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity reg_file is
+entity fwd_unit is
     port(
-        clk : in std_logic;
-        write_en : in std_logic;
+        reg_s1 : in std_logic_vector(4 downto 0);
+        reg_s2 : in std_logic_vector(4 downto 0);
+        reg_dst : in std_logic_vector(4 downto 0);
 
-        write_data : in std_logic_vector(31 downto 0);
-        
-        write_addr : in std_logic_vector(4 downto 0);
-        reg_a_addr : in std_logic_vector(4 downto 0);
-        reg_b_addr : in std_logic_vector(4 downto 0);
+        mem_wb_reg_dst : in std_logic_vector(4 downto 0);
+        ex_mem_reg_dst : in std_logic_vector(4 downto 0);
 
-        reg_a_data : out std_logic_vector(31 downto 0);
-        reg_b_data : out std_logic_vector(31 downto 0)
+        -- flags control ex/mem e mem/wb
+        ex_mem_reg_w : in std_logic;
+        mem_wb_reg_w : in std_logic;
+
+        forward_a : out std_logic_vector(1 downto 0);
+        forward_b : out std_logic_vector(1 downto 0);
+        forward_c : out std_logic
     );
-end reg_file;
+end fwd_unit;
 
-architecture Behavioral of reg_file is
-    -- 32 registradores de 32 bits
-    type register_mem is array (integer range <>) of std_logic_vector(31 downto 0);
-    signal register_bank : register_mem(0 to 31) := (others => x"00000000");
+architecture Behavioral of fwd_unit is
+    begin
+        forward_decode : process(reg_s1, reg_s2, reg_dst, mem_wb_reg_dst, ex_mem_reg_dst, ex_mem_reg_w, mem_wb_reg_w) is
+            begin
+            	 forward_a <= "00";
+            	 forward_b <= "00";
+            	 forward_c <= '0';
+            	
+                if (ex_mem_reg_w = '1') and (ex_mem_reg_dst /= "00000") then
+                    if (ex_mem_reg_dst = reg_s1) then
+                        forward_a <= "10";
+                    end if;
+                    if (ex_mem_reg_dst = reg_s2) then
+                        forward_b <= "10";
+                        forward_c <= '1';
+                    end if;
+                end if;
+			 
+                if (mem_wb_reg_w = '1') and (mem_wb_reg_dst /= "00000") and not (ex_mem_reg_w ='1' and (ex_mem_reg_dst /= "00000") and (ex_mem_reg_dst = reg_s1)) and (mem_wb_reg_dst = reg_s1) then
+                    forward_a <= "01";
+                end if;
+                if (mem_wb_reg_w = '1') and (mem_wb_reg_dst /= "00000") and not (ex_mem_reg_w ='1' and (ex_mem_reg_dst /= "00000") and (ex_mem_reg_dst = reg_s2)) and (mem_wb_reg_dst = reg_s2) then
+                    forward_b <= "01";
+                end if;
 
-    begin       
-        register_access: process(clk, write_en, write_addr) is
-        begin
-            -- na borda de subida
-            if rising_edge(clk) and write_en = '1' then
-            	if write_addr /= "00000" then
-            		register_bank(to_integer(unsigned(write_addr))) <= write_data;
-            	end if;
-            end if;
-            if falling_edge(clk) then
-            	reg_a_data <= register_bank(to_integer(unsigned(reg_a_addr)));
-        	reg_b_data <= register_bank(to_integer(unsigned(reg_b_addr)));
-            end if;
-        end process register_access;
-
+        end process forward_decode;
 end Behavioral;
