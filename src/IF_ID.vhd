196faddf0800d18482c1df20a125f3fc62ee20f3
Leandro Assis <73458591+leandroassis@users.noreply.github.com>
2023-07-12 14:27:53 -0300

    Update IF_ID.vhd

--- src/IF_ID.vhd
+++ src/IF_ID.vhd
@@ -6,11 +6,12 @@ entity IF_ID is
         clk : in std_logic;
         instr_in: in std_logic_vector(31 downto 0);
         pc_in : in std_logic_vector(31 downto 0);
-        if_id_write : in std_logic;
-        if_id_flush : in std_logic;
 
         instr_out: out std_logic_vector(31 downto 0);
-        pc_out : out std_logic_vector(31 downto 0)
+        pc_out : out std_logic_vector(31 downto 0);
+
+	    if_id_write : in std_logic;
+        if_id_flush : in std_logic
     );
 end IF_ID;
 
